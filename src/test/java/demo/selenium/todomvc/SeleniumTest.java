ebc781aadc0c06c58f3548ab328590a8410160b5
Paul Shirmovski <pshirmovski@gmail.com>
2020-04-25 22:21:09 +0200

    initial setup

---
+++ src/test/java/demo/selenium/todomvc/SeleniumTest.java
@@ -0,0 +1,18 @@
+package demo.selenium.todomvc;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@ExtendWith(SeleniumExtension.class)
+class SeleniumTest {
+
+    @Test
+    void projectIsConfigured(ChromeDriver driver) {
+
+
+    }
+
+
+}
