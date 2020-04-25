b38617c429f5f7bf3c7af9f925ecee8d00d9f39d
Paul Shirmovski <pshirmovski@gmail.com>
2020-04-26 01:35:04 +0200

    complete demo

---
+++ src/main/java/pages/TodoMvc.java
@@ -0,0 +1,21 @@
+package pages;
+
+import java.util.List;
+
+public interface TodoMvc {
+
+    void navigateTo();
+    void createTodo(String todoName);
+    void createTodos(String... todoNames);
+    int getTodosLeft();
+    boolean todoExists(String todoName);
+    int getTodoCount();
+    List<String> getTodos();
+    void renameTodo(String todoName, String newTodoName);
+    void removeTodo(String todoName);
+    void completeTodo(String todoName);
+    void completeAllTodos();
+    void showActive();
+    void showCompleted();
+    void clearCompleted();
+}
